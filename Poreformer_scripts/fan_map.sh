105f744176b930e55193ffd1213884981e6b72ae
Bessyyi <73151161+Bessyyi@users.noreply.github.com>
2024-05-05 22:01:31 +0800

    Delete Poreformer_scripts/fan_map.sh

--- Poreformer_scripts/fan_map.sh
+++
@@ -1,4 +0,0 @@
-python3 /home/yzy/code/fast5_fastq.py $1
-python3 /home/yzy/code/fast5_dian.py $1 > ${1}.txt
-python3 /home/yzy/code/dian_first_step.py $1
-#python3 /home/yzy/code/out-dianliu1.py $1
