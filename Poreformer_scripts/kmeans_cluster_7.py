4edc38f18196e4bc8b6e3d656bfb8bc8b912d14b
Bessyyi <73151161+Bessyyi@users.noreply.github.com>
2024-05-19 22:21:04 +0800

    Add files via upload

---
+++ Poreformer_scripts/kmeans_cluster_7.py
@@ -0,0 +1,32 @@
+import fileinput
+import numpy as np
+import sys
+input_file1 = sys.argv[1]
+input_file2 = sys.argv[2]
+
+
+dict1 = {}
+for i in fileinput.input(files=(input_file1)):
+    j = i.strip().split(',')
+    m = j[0].split('pos')[1]
+    dict1[m] = j[1:]
+    # print(j[0],j[1],jn1,jn2,jn3,jn4,jn5)
+
+    # print(dict1)
+# h = 0
+list1 = []
+dict2 ={}
+for i in fileinput.input(files=(input_file2)):
+    j = i.strip().split(' ')
+    n = j[1].split('pos')[1]
+    dict2[n]= []
+    for m in range(int(n) - 7, int(n) + 8 ):
+        #dict2[j].append(dict1[str(m)])
+        if str(m) in dict1.keys():
+            a = dict1[str(m)]
+            #print(a)
+            dict2[n].append(','.join(a))
+#print(dict2)
+for i in dict2.keys():
+    a = ','.join(dict2[i])
+    print('pos'+i+','+a)
