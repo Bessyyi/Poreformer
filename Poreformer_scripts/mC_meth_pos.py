17aff4dde3925c40f73020d5a8a23fb038a6f556
Bessyyi <73151161+Bessyyi@users.noreply.github.com>
2024-05-19 22:31:49 +0800

    Add files via upload

---
+++ Poreformer_scripts/mC_meth_pos.py
@@ -0,0 +1,14 @@
+import sys
+in_file=sys.argv[1]
+with open (in_file) as f:
+    m=""
+    for i in f:
+        m+=i
+with open("mC_forward_pos.txt","w") as fw,open("mC_forward_pos_5.txt")as fw1:
+    for pos in range(5,len(m)-5):
+        if m[pos] =="A":
+			print("pos"+str(pos),file=fw)
+            for n1 in range(5,0,-1):
+                print("pos"+str(pos-n1),file=fw1)
+            for n2 in range(6):
+                print("pos"+str(pos+n2),file=fw1)
